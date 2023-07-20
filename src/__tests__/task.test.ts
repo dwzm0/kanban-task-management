40646f5d244ec9feaddf96d708a39eec13730922
dwzm0 <dwzm00@gmail.com>
2023-07-20 15:15:47 +0300

    task get route tests

--- src/__tests__/task.test.ts
+++ src/__tests__/task.test.ts
@@ -35,10 +35,39 @@ describe('task', () => {
         describe('given col and task exits', () => {
             it('should return 200', async () => {
                 const {statusCode, body} = await supertest(app)
-                .get(`/api/dashboards/${dashboard._id}/columns/${dashboard.columns[0]._id}tasks/${dashboard.columns[0].tasks[0]._id}`);
+                .get(`/api/dashboards/${dashboard._id}/columns/${dashboard.columns[0]._id}/tasks/${dashboard.columns[0].tasks[0]._id}`);
                 expect(statusCode).toBe(200);
-                console.log(body);
+                expect(body.title).toEqual(dashboard.columns[0].tasks[0].title);
+            });
+        });
 
+        describe('given task doesnt exit', () => {
+            it('should return 404', async () => {
+                const fakeTask = '64ad951d1985a2de99048781';
+                const {statusCode, body} = await supertest(app)
+                .get(`/api/dashboards/${dashboard._id}/columns/${dashboard.columns[0]._id}/tasks/${fakeTask}`);
+                expect(statusCode).toBe(404);
+                expect(body.error).toEqual(`Task by ID ${fakeTask} does not exist`);
+            });
+        });
+
+        describe('given task doesnt exit', () => {
+            it('should return 404', async () => {
+                const fakeCol = '64ad951d1985a2de99048781';
+                const {statusCode, body} = await supertest(app)
+                .get(`/api/dashboards/${dashboard._id}/columns/${fakeCol}/tasks/${dashboard.columns[0].tasks[0]._id}`);
+                expect(statusCode).toBe(404);
+                expect(body.error).toEqual(`Column by ID ${fakeCol} does not exist`);
+            });
+        });
+
+        describe('given task doesnt exit', () => {
+            it('should return 404', async () => {
+                const fakeDashboard = '64ad951d1985a2de99048781';
+                const {statusCode, body} = await supertest(app)
+                .get(`/api/dashboards/${fakeDashboard}/columns/${dashboard.columns[0]._id}/tasks/${dashboard.columns[0].tasks[0]._id}`);
+                expect(statusCode).toBe(404);
+                expect(body.error).toEqual(`Blog by ID ${fakeDashboard} does not exist`);
             });
         });
     });
