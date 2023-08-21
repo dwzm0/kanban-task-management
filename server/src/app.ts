350b152dc66a7b69444937dbb1b5af92c80365b7
dwzm0 <dwzm00@gmail.com>
2023-08-21 18:27:44 +0300

    deleted node config folder

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,7 +8,6 @@ const port = appConfig.get<number>("port");
 
 export const app = createServer();
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
 app.listen(port, async () => {
   logger.info(`Server running on port ${port}`);
 
