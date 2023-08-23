400caad9064fdd4595e7e177a3008cd5d7cd3373
dwzm0 <dwzm00@gmail.com>
2023-08-23 14:06:26 +0300

    production config dir path 7

--- server/build/config/production.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-process.env["NODE_CONFIG_DIR"] = __dirname;
-
-dotenv.config();
-
-export default {
-    port: 3001,
-    dbUri: `mongodb+srv://dwzm00:${process.env.PASSWORD}@cluster0.ii6blwz.mongodb.net/Dashboard?retryWrites=true&w=majority`,
-};
